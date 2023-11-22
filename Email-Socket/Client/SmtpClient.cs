7104358864c8746f99cd46d505bf9192969247f9
MavisDo <do.mavis02@gmail.com>
2023-11-22 18:42:00 +0700

    Restructure SMTP class and added SendEmail() method

--- Email-Socket/Client/SmtpClient.cs
+++ Email-Socket/Client/SmtpClient.cs
@@ -2,99 +2,126 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.IO;
-using Config;
+using EmailSocket;
+using EmailHandler;
+using System.Security.Cryptography;
+using System;
 
 namespace SMTP
 {
     class Smtp_Client
     {
-        public void Connection() {
-            // Get file json path
-            string file = Path.GetFullPath("Config.json");
-            ConfigJson.Load(file);
-            // Specify the SMTP server and port
-            IPAddress MailServer = IPAddress.Parse(ConfigJson.General!.MailServer!);
-            short SmtpPort = ConfigJson.General.SmtpPort;
+        private Socket socket;
+        private NetworkStream? networkStream;
+        private StreamReader? streamReader;
+        private StreamWriter? streamWriter;
+        public IPAddress server;
+        public short port;
 
-            // Create a socket and connect to the SMTP server
-            using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(MailServer, SmtpPort);
+        public Smtp_Client()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ConfigJson config = ConfigJson.Load(Path.GetFullPath("Config.json"));
+            server = IPAddress.Parse(config.General!.MailServer!);
+            port = config.General!.SmtpPort!;
+        }
 
-            // Specify sender and recipient email addresses
-            string tmp = ConfigJson.General.Username!;
-            string from = tmp.Substring(tmp.IndexOf('<'), tmp.Length - 1 - tmp.IndexOf('<'));
-            string to = "phuochoan17032004@gmail.com, ahihi@testing.com";
-            string[] Recipient = AdequateMail(to);
+        public Smtp_Client(string server, short port)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            this.server = IPAddress.Parse(server);
+            this.port = port;
+        }
+
+        public void Connect()
+        {
+            socket.Connect(server, port);
+            networkStream = new NetworkStream(socket);
+            streamReader = new StreamReader(networkStream, Encoding.ASCII);
+            streamWriter = new StreamWriter(networkStream, Encoding.ASCII);
+            streamWriter.AutoFlush = true;
 
             // Check if the connection is successful
             if (!socket.Connected)
-            {
-                Console.WriteLine("Failed to connect to the SMTP server.");
-                return;
-            }
+                throw new Exception("Failed connecting to server!");
+        }
 
-            using NetworkStream networkStream = new NetworkStream(socket);
-            using StreamReader reader = new StreamReader(networkStream, Encoding.ASCII);
-            using StreamWriter writer = new StreamWriter(networkStream, Encoding.ASCII);
-            writer.AutoFlush = true;
-
-            // Read the welcome message from the server
-            string? welcomeMessage = reader.ReadLine();
-            Console.WriteLine(welcomeMessage);
-
-            // Send the EHLO command to the server
-            writer.WriteLine("EHLO example.com");
-
-            // Read and print the response
-            string? ehloResponse = reader.ReadLine();
-            Console.WriteLine(ehloResponse);
-
-            // Send the MAIL FROM command
-            writer.WriteLine($"MAIL FROM:<{from}>");
-
-            // Read and print the response
-            string? mailFromResponse = reader.ReadLine();
-            Console.WriteLine(mailFromResponse);
-
-            // Send the RCPT TO command
-            writer.WriteLine($"RCPT TO:<{to}>");
-
-            // Read and print the response
-            string? rcptToResponse = reader.ReadLine();
-            Console.WriteLine(rcptToResponse);
-
-            // Send the DATA command
-            writer.WriteLine("DATA");
-
-            // Read and print the response
-            string? dataResponse = reader.ReadLine();
-            Console.WriteLine(dataResponse);
-
-            /* Send the email content */            
-
-            // Read and print the response
-            string? sendDataResponse = reader.ReadLine();
-            Console.WriteLine(sendDataResponse);
-
-            // Send the QUIT command
-            writer.WriteLine("QUIT");
-
-            // Read and print the response
-            string? quitResponse = reader.ReadLine();
-            Console.WriteLine(quitResponse);
-
-            networkStream.Close();
-            writer.Close();
-            reader.Close();
+        public void Close()
+        {
+            networkStream!.Close();
+            streamWriter!.Close();
+            streamReader!.Close();
             socket.Close();
         }
-        private string[] AdequateMail(string mail) {
+
+        private string[] AdequateMail(string mail)
+        {
             // To get each mail receiver adequately
             string[] mails = mail.Split(",");
-            for (int i = 0; i < mails.Length; ++i) {
+            for (int i = 0; i < mails.Length; ++i)
+            {
                 mails[i] = mails[i].Trim();
             }
             return mails;
         }
+
+        public void SendEmail(Email email)
+        {
+            if (!socket.Connected)
+                throw new Exception("Haven't connected to the server!");
+
+            streamWriter!.WriteLine("EHLO example.com");
+            streamWriter.WriteLine($"MAIL FROM:<{email.From}>");
+
+            SortedSet<string> peopleToSend = new SortedSet<string>();
+            foreach (string to in email.To)
+                peopleToSend.Add(to);
+            foreach (string cc in email.CC)
+                peopleToSend.Add(cc);
+            foreach (string bcc in email.BCC)
+                peopleToSend.Add(bcc);
+            foreach (string to in peopleToSend)
+                streamWriter.WriteLine($"RCPT TO:<{to}>");
+
+            // Begin to send email content
+            streamWriter.WriteLine("DATA");
+            string randomBoundary = string.Format("----------{0:N}", Guid.NewGuid());
+            streamWriter!.Write(Mime.GetMainHeader(email, randomBoundary));
+
+            if (email.Attachments.Count == 0)
+                streamWriter.Write(email.Body);
+            else
+            {
+                streamWriter.Write(Mime.GetMimePartHeader(randomBoundary, null, false));
+                streamWriter.WriteLine(email.Body);
+                streamWriter.WriteLine();
+                foreach (string attachment in email.Attachments)
+                {
+                    // Send header of each files
+                    streamWriter.Write(Mime.GetMimePartHeader(randomBoundary, attachment));
+                    SendFile(attachment);
+                }
+            }
+            streamWriter.WriteLine();
+            streamWriter.WriteLine($"{randomBoundary}--");
+            streamWriter.WriteLine(".");
+        }
+
+        private void SendFile(string filePath)
+        {
+            byte[] buffer = new byte[72];
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            using (CryptoStream b64Stream = new(fileStream, new ToBase64Transform(), CryptoStreamMode.Read))
+            {
+                int read;
+                while ((read = b64Stream.Read(buffer)) != 0)
+                {
+                    networkStream.Write(buffer, 0, read);
+                    networkStream.Write("\r\n"u8.ToArray());
+                }
+            }
+        }
     }
+
 }
